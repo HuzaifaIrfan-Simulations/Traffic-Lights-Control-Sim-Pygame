bb8b852050ab598b7a4a85e7abf19fe03a4373c9
Huzaifa Irfan <huzaifairfan2001@gmail.com>
2020-09-23 20:08:38 +0500

    carr movement and control

---
+++ app_settings.py
@@ -0,0 +1,66 @@
+
+
+
+
+
+# settings used in app.py
+carmovepersecond=1
+
+mincargen=0
+
+maxcargen=25
+
+
+
+# settings used in Car.py
+
+carsize = 40
+
+
+setcaracc=False
+caracc=1
+
+setcarvel=True
+carvel=2
+
+
+# settings used in Light.py
+
+defaultyellowlighttime=2
+
+defaultgreenlighttime=10
+
+
+useautocar=True
+
+carcountsettings= [
+
+    {
+        "mincars":20,
+        "yellow":5,
+        "green":10
+    },
+
+    {
+        "mincars":10,
+        "yellow":2,
+        "green":5
+    },
+
+    {
+        "mincars":5,
+        "yellow":2,
+        "green":5
+    },
+
+    {
+        "mincars":0,
+        "yellow":2,
+        "green":5
+    }
+
+    ]
+
+
+
+
